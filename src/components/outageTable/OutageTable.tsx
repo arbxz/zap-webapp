e417b15ec94e781d6d6b9dcbd530a4e342c33873
Arbaaz Mowlabucus <91611231+arbxz@users.noreply.github.com>
2025-03-27 21:55:20 +0400

    Security fix

--- src/components/outageTable/OutageTable.tsx
+++ src/components/outageTable/OutageTable.tsx
@@ -16,19 +16,6 @@ const OutageTable = ({ data, selectedRegion }: OutageTableProps) => {
   const [todayData, setTodayData] = useState<OutageItem[]>([]);
   const [futureData, setFutureData] = useState<OutageItem[]>([]);
 
-  function formatDate(dateString: string) {
-    const date = new Date(dateString);
-
-    const day = date.getDate().toString().padStart(2, "0");
-    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // getMonth() is zero-based
-    const year = date.getFullYear().toString().substr(-2); // Get last 2 digits of year
-
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
-
-    return `${day}/${month}/${year} ${hours}:${minutes}`;
-  }
-
   useEffect(() => {
     const { today, future } = data;
 
